dfdad1819fb676179fef2615a6d266e016130fed
Chandrashish Prasad <61428395+Chandrashish@users.noreply.github.com>
2021-06-27 00:14:43 +0530

    Add files via upload

---
+++ reducer3.py
@@ -0,0 +1,39 @@
+from operator import itemgetter
+import sys
+
+current_headline = None
+current_count = 0
+heading = None
+
+# input comes from STDIN
+for line in sys.stdin:
+    # remove leading and trailing whitespace
+    headline = line.strip()
+
+    # parse the input we got from mapper.py
+    heading, count = headline.split('\t', 1)
+
+    # convert count (currently a string) to int
+    try:
+        count = int(count)
+    except ValueError:
+        # count was not a number, so silently
+        # ignore/discard this line
+        continue
+
+    # this IF-switch only works because Hadoop sorts map output
+    # by key (here: word) before it is passed to the reducer
+    if current_headline == heading:
+        current_count += count
+    else:
+        if current_headline:
+            # write result to STDOUT
+            if current_count>1:
+                print('%s\t%s' % (current_headline, current_count))
+        current_count = count
+        current_headline = heading
+
+# do not forget to output the last word if needed!
+if current_headline == heading:
+    if current_count>1:
+        print('%s\t%s' % (current_headline, current_count))
